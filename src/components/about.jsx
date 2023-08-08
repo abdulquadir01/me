fbf9c1a2f70b67177551d2bd9a8474ae08fa392e
AbubakrRehman <abubakr.rehman91@outlook.com>
2023-08-08 19:06:36 +0530

    updated exprience & project done count

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -30,13 +30,13 @@ export const About = () => {
                         <article className='about-card'>
                             <FaAward className='about-icon' />
                             <h5>Experience</h5>
-                            <small>2+ Years Working</small>
+                            <small>3+ Years Working</small>
                         </article>
 
                         <article className='about-card'>
                             <VscFolderLibrary className='about-icon' />
                             <h5>Projects</h5>
-                            <small>10+ Completed</small>
+                            <small>20+ Completed</small>
                         </article>
                     
                     </div>
